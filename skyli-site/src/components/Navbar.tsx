432be3dc4000efe947950c0d90b599ef8cb5aafd
maxgunnar <maxgyllingmusic@gmail.com>
2025-08-11 22:38:31 +0200

    SKYLI-006 (Style): Increase letter spacing on navbar li-elements

--- skyli-site/src/components/Navbar.tsx
+++ skyli-site/src/components/Navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar({ brand, links, className = "" }: NavbarProps) {
       <ul className="flex uppercase space-x-6 md:space-x-12 lg:space-x-32 justify-center">
         {links.map((link) => (
           <li key={link.path}>
-            <Link to={link.path} className="relative pb-1 border-b-2 border-transparent hover:border-white transition-all duration-300">
+            <Link to={link.path} className="relative tracking-widest pb-1 border-b-1 border-transparent hover:border-white transition-all duration-300">
               {link.label}
             </Link>
           </li>
